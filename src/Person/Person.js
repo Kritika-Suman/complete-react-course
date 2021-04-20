dc60f9d5ba541e663eaa7acad43dfad710650d28
kritika suman <kritikasuman2016@gmail.com>
2021-04-20 21:03:52 +0530

    Learned and implemented advanced css feature like media query with inline styling using radium package.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,8 +4,17 @@ import './Person.css';
 
 const person = (props) => {
 
+    const style = {
+           '@media (min-width: 500px)': {
+                width: '450px'
+           }
+           // If we resize the application, boxes will stay in the middle and will not grow in width anymore.
+           // They will be growing in width to 60% of the size though, if we go below the media query threshold,
+           // which is 500 px otherwise it will stay fixed with 450 px
+    };
+
     return(
-        <div className="Person">
+        <div className="Person" style={style}>
             <p onClick={props.click}>I'm {props.name} I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" value={props.name} onChange={props.changed}/>
