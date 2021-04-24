6d392bdaef6bdd0f2ee2e3daeb6be81fe9696566
kritika suman <kritikasuman2016@gmail.com>
2021-04-24 11:56:43 +0530

    Learned and Used styled componenets.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import './App.css';
-import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -127,25 +126,23 @@ class App extends Component {
           }
 
         return (
-            <StyleRoot>
-                <div className="App">
-                    <h1>Hi, I'm a React App</h1>
-                    <p className={classes.join(' ')}>This is really working!</p>
-                    <button
-                    style={style}
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p className={classes.join(' ')}>This is really working!</p>
+                <button
+                style={style}
 //                    onClick={() => this.switchNameHandler("Kritika!!")}
-                      onClick={this.togglePersonsHandler}
-                    >
-                        Toggle Persons
-                    </button>
-                    {persons}
-                </div>
-            </StyleRoot>
+                  onClick={this.togglePersonsHandler}
+                >
+                    Toggle Persons
+                </button>
+                {persons}
+            </div>
           );
 //        return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a React App'));
     }
 
 }
 
-export default Radium(App); // called higher order component
+export default App; // called higher order component
                             // valid for both class based component and functional component
